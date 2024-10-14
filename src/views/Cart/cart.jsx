09f81de8db08f86320a8f39e809f9c554b6c4a14
ansourian <jairansourian@gmail.com>
2024-10-14 10:10:14 -0300

    cart semi funcional

--- src/views/Cart/cart.jsx
+++ src/views/Cart/cart.jsx
@@ -1,12 +1,17 @@
+import { useContext } from "react"
+import { CartContext } from "../../context/CartContext"
+
 export default function Cart() {
-    return (
-      <main>
-        <h2>Carrito</h2>
-        <hr className="linea-subtitulo"></hr>
-        <p className="p-sub">
-          Acá podes visualizar los productos que tenes agregados al carrito
-        </p>
-        <hr></hr>
-      </main>
-    );
-  }
+  const [cart] = useContext(CartContext)
+  return (
+    <main>
+      <h2>Carrito</h2>
+      <hr className="linea-subtitulo"></hr>
+      <p className="p-sub">
+        Acá podes visualizar los productos que tenes agregados al carrito
+      </p>
+      <hr></hr>
+      {cart.length ? cart.map((e)=> <p>{e.title}</p>) : <p>No hay productos en tu carrito.</p>}
+    </main>
+  )
+}
